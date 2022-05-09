b72f518625561b812ea9e8f69f4f3621f9a54bdc
md-maruf-hossain <sany.maruf@gmail.com>
2022-05-09 13:24:50 +0600

    configured BrowserRouter and withRouter with product object

--- src/index.js
+++ src/index.js
@@ -1,16 +1,81 @@
+// Importing required libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MyWeb3 from 'web3'
+import { BrowserRouter, Route, withRouter } from 'react-router-dom'
 
 class Main extends React.Component {
-    constructor() {
-        super()
+    /* Creating constructor with few products that have necessary properties
+    to display the greatest amount of information to users as possible.
+    Properties such as 'title', 'description', 'id' and 'price' are a must.*/
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            products: {
+                id: 1,
+                title: 'Garnised salad',
+                date: Date.now(),
+                owner: '',
+                price: 12,
+                image: 'https://cdn.shopify.com/static/sample-images/garnished.jpeg'
+            },
+            products: {
+                id: 2,
+                title: 'Classic Teapot',
+                date: Date.now(),
+                owner: '',
+                price: 15,
+                image: 'https://cdn.shopify.com/static/sample-images/teapot.jpg'
+            },
+            products: {
+                id: 3,
+                title: 'Trendy Shoe',
+                date: Date.now(),
+                owner: '',
+                price: 18,
+                image: 'https://cdn.shopify.com/static/sample-images/shoes.jpeg'
+            },
+            products: {
+                id: 4,
+                title: 'Generic Bath',
+                date: Date.now(),
+                owner: '',
+                price: 30,
+                image: 'https://cdn.shopify.com/static/sample-images/bath.jpeg'
+            },
+            productsHtml: [],
+            productDetails: [],
+            product: {},
+        }
+    }
+
+    //This function converts strings into valid hexadecimal.
+    bytes32(name) {
+        return MyWeb3.utils.fromAscii(name)
     }
 
     render() {
         return (
-            <div>The project has been setup. Remember to npm install and webpack -w -d to see your changes.</div>
+            <div>
+                <Route path="/" exact render={() => (
+                    <div>The dApp has been setup</div>
+                )} />
+            </div>
         )
     }
 }
 
-ReactDOM.render(<Main />, document.querySelector('#root'))
+/*To be able to access the history in order to redirec users
+programmatically when opening a product*/
+Main = withRouter(Main)
+
+ReactDOM.render(
+    /* Adding the 'BrowserRouter' component from the react router
+    to initialize the router object*/
+    /*'BrwoserRouter' component is the main component that is used 
+    to initialize the router so that they can manage different pages.*/
+    <BrowserRouter>
+        <Main />
+    </BrowserRouter>,
+document.querySelector('#root'))
